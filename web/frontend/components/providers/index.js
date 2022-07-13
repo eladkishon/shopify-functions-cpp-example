e8fc1592e5895e9ffee1b2f398f10a6c2f6226cd
lirzetouny@gmail.com <lirzetouny@gmail.com>
2022-07-13 18:34:45 +0300

    add front

---
+++ web/frontend/components/providers/index.js
@@ -0,0 +1,5 @@
+export { AppBridgeProvider } from "./AppBridgeProvider";
+export { GraphQLProvider } from "./GraphQLProvider";
+export { PolarisProvider } from "./PolarisProvider";
+export { DiscountProvider } from './DiscountProvider'
+
